b5d8ebb3160c487ecce3eda71d56d35de0d1a5d7
John <johnmulvey48@yahoo.com>
2022-09-13 16:06:28 -0400

    git@github.com:Jmulvez/phase-3-active-record-crud-operations-lab.git

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,18 @@
 class Movie < ActiveRecord::Base
-
-end
+    def self.find_all_movies_by_year(year)
+      Movie.where(year: year)
+    end
+    def self.create_with_title(title)
+        Movie.find(title: title)
+    end
+    def self.first_movie(title)
+        Movie.find(title: title)
+    end
+    def self.last_movie(title)
+        Movie.find(title: title)
+    end
+    def self.movie_count(movie)
+        Movie.count(movie)
+    end
+    
+  end
